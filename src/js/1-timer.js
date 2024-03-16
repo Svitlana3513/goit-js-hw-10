2499ca50e62dcd3afaced64a5aff126db5893e04
Svitlana Saharieva <149310522+Svitlana3513@users.noreply.github.com>
2024-03-16 16:30:18 +0000

    add

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,13 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-
-const calendarDate = flatpickr(`#datetime-picker`, options);
-const inputDate = document.querySelector(`#datetime-picker`);
-const startBtn = document.querySelector(`button`);
-
-
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -17,3 +10,12 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+
+const calendarDate = flatpickr(`#datetime-picker`, options);
+const inputDate = document.querySelector(`#datetime-picker`);
+const startBtn = document.querySelector(`button`);
+
+
+
+
